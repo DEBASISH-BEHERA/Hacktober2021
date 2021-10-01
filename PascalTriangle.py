0fd98bac7d2192a4bce58be05559d2eabd41e71b
Paulchen <lukas.23022005@gmail.com>
2021-10-01 17:35:19 +0200

    Update PascalTriangle.py
    Paulchen5:
    merge line 4&5 into one
    update print function in line12/13

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,8 +1,7 @@
 n=int(input("Enter number of rows: "))
 a=[]
 for i in range(n):
-    a.append([])
-    a[i].append(1)
+    a.append([1])
     for j in range(1,i):
         a[i].append(a[i-1][j-1]+a[i-1][j])
     if(n!=0):
@@ -10,5 +9,5 @@ for i in range(n):
 for i in range(n):
     print("   "*(n-i),end=" ",sep=" ")
     for j in range(0,i+1):
-        print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
+        print(f'{a[i][j]}     ',end=" ",sep=" ")
     print("\n")
