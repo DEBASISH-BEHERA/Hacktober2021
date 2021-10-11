fdba6670ca05cf80ad9dab06eb0bb46d05f3acdf
laulak123 <92267240+laulak123@users.noreply.github.com>
2021-10-11 09:29:13 +0530

    square_root

---
+++ squareroot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ int mySqrt(int x) {
+        int ans=0;
+        
+        ans=sqrt(x);
+        return ans;
+    }
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<mySqrt(x);
+    return 0;
+
+    return 0;
+}
