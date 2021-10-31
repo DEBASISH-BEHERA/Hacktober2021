7a52f05ecad916140026dc087fa50beaf590e8bb
aishwarya416 <72721047+aishwarya416@users.noreply.github.com>
2021-10-31 19:09:18 +0530

    Pyramid Patterns in C

---
+++ PyramidPatterns.c
@@ -0,0 +1,18 @@
+#include <stdio.h>  
+#include <conio.h>  
+void main()  
+{  
+    int i, j, rows;  
+    printf (" Enter a number to define the rows: \n ");  
+    scanf("%d", &rows);  
+    printf("\n");  
+    for (i = 1; i <= rows; ++i) // outer loop  
+    {  
+        for (j = 1; j <= i; ++j) // inner loop  
+        {  
+            printf ("* "); // print the Star  
+        }  
+        printf ("\n");   
+    }  
+    getch();      
+}  
